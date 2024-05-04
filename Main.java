96e81a940bf79ab23312fe5603cbecb21eccc9aa
Impala67SS <162022376+Impala67SS@users.noreply.github.com>
2024-05-04 12:11:26 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import company.beans.Person;
+import exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
+import company.utils.MathUtils;
+
+public class Main {
+    public static void main(String[]args) {
+        try {
+            Person person = new Person("Kamil", 23);
+            System.out.println("Name:"+ person.getName());
+            System.out.println("Age: "+person.getAge());
+
+            int sum = MathUtils.add(12, 50);
+           
+
+            Messenger messenger = new EmailMessenger();
+            String message = "Dobry Den. Suma: " + sum;
+            messenger.sendMessage(message);
+
+        } catch (InvalidAgeException e) { System.out.println("Invalid age provided"+ e.getMessage());
+    
+    }
+    }
+    
+}
